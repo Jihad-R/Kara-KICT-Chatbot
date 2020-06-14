5540f9554a10bf27e8e5b90a6106b0e138df47d7
JihadMirinda <Gifth22@gmail.com>
2020-06-14 16:33:52 +0800

    initial commit

---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "kara-general-nwcswd",
+  private_key_id: "43eddfd9c1a6cc8304792bcbfbed21eb2a5cd2df",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCbauDa0kV9Aq7J\nRmDuzMCTGlcqYkV9Ij41wRsDVZWpWZ5bAQp7Y4mqg14Dl7IIHVuTlHY72uFg0isw\nro3I28nlqG8hXoZk6UxF+tgYWcLO6bnVViadKU+ld1N2nzwFKZGu0CxKxOZgRYHE\nvhsC5xMzpD9Vf5RYGMW8HSzPMwO9gLDc60HSHjCkp3tmvTWPskVNGrxjQQQOaL16\nQfTPc3shL/V4A0lsFxRfqVIxnkPL7v8a6Af9Zo1/xES17iVN7qGOiuzCW0EaF/gs\nBiPYj8XtjLeq7U9Rc6nQW3HTh7TwBvgzSovRLgHO0PntihbhjP6y9c0Aj45WF/3E\nI2Qg+HMrAgMBAAECggEATF+ltrKdBjUiynMk4ETLH57Sldfr+PWyZ30JNsbAgJOz\nL1pgGcmM1bevpAlQKg+vOGM9BOXnWkp9gfNNZpanbQ2r6tSGK6G/4yXFOpYllsOh\nmDY5TIMICjKM196hQpIubp8LHQUMhuOxRQr8wBzowThmDTBgKDioNuyJYBrcLCuE\n8gULxfzvxsfEuxsU3CbWfc9MaRakQb+CI8+K7HPGXH21Sb9agL7TU5XiUlUXelkP\nlFyf3E9mXqyqqJDMEaAb0ydeGzy245abV0WNwHSjrV4HZm2d6NhJvuOlp1HiICxI\nUdNx3mz9BnZWHd2j69TYb+Ap1hYWZx+joT+rE8UwBQKBgQDI/LF4VVK38MsbLMka\nP9PGNjBE0smWPWqtn6cPzXxy7fhSTgOGn907l2ABrESO1rltwdlvgUXmtvQ+B/O5\nmA/Y+51F7zAxKqHYn4JU8gD4/4ucZ8Ao9DCHWTtcuWK5A9kFbryJani+2Vr4C8/e\nM1htm8jIro9BD/zC9zfLm5qJ9QKBgQDF9ReAOssBHuvD1OyA+CORVtG2Vo3TjUsG\nSvM7vycGeyGGbtPyws3IwQb+QWZEiUo8UYwv9OflARdFGM76iKrIJor2nZoFSxbK\nwSA4NVEgo9vcC3M5owhY8SugwOnH3EdA/RTjv0GtHQ9NYh3rOudWKcaNndrLeEGH\nmqkhyrs0nwKBgQC2YlBtgQfKMTEuHMr1DfyP/rZ4jcsukdcPbTQ9q9uFs5rGnVKn\nxMQOgoQ1oGyqLgIgrmswu1+CSH2FGNQ7k9i1r0Z9otrZkvVBTSYJDX6Cderr6F5B\nC65J9FhhAT2r5Oj3/BAMptihXz4knxtnaLKS7G4cZgNLKEXXal1wX2iyTQKBgQDD\ncb13NBT0EVRTL+klWFi9iUtaTFCQ2wKEvwExLwRXyMAL22ZVW3ITUEkYRPe0mRv7\nO8Q+IP3HQvF3ngvUnZSIMk2nOWk06rMhEdY+yJ1FraJHueJUuJtoPQyHFZB7bAfJ\nqVWzmkQ8p+FQ1JuXgLugfXR0o7K+iwef29o6688zHwKBgC6s1QZ2JQ2saGZbjE+v\niuUeI3XWQBUvmmQ6ZHtL/PbAzD+EahVaBhaXaKXe2uxY47gzD0o2LLA519iDp+zO\nPNt/5slxTu1CqCBAaQJY4u3D/TMj0QtljWpU08Wkmierxsh45GMtjqbnrq22Z8at\n7tJHviwFqFPIEvrQ/iVhroz8\n-----END PRIVATE KEY-----\n",
+  client_email: "dialogflow-jqcvsy@kara-general-nwcswd.iam.gserviceaccount.com",
+  client_id: "101239571977818728726",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-jqcvsy%40kara-general-nwcswd.iam.gserviceaccount.com",
+};
